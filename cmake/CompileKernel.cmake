91faf0141505be3ec0ca087cc0d75dc5456de27f
Yong <matrixpool@163.com>
2025-01-20 14:31:05 +0800

    更新linux内核安装脚本

--- cmake/CompileKernel.cmake
+++ cmake/CompileKernel.cmake
@@ -1,7 +1,13 @@
 include(ExternalProject)
 
+if("${QSG_CPU}" STREQUAL "x86_64")
+  set(__ARCH "x86")
+endif()
+
 set(QSG_KERNEL_DIR "${QSG_BUILD_DIR}/linux-${QSG_KERNEL_VERSION}" CACHE STRING "QSG kernel source directory" FORCE)
-message_yellow("linux kernel: ${QSG_KERNEL_DIR}")
+message_yellow("linux kernel version:   ${QSG_KERNEL_VERSION}")
+message_yellow("linux kernel path:      ${QSG_KERNEL_DIR}")
+
 #compile kernel
 ExternalProject_Add(kernel
     URL ${__KERNEL_URL}
@@ -12,7 +18,11 @@ ExternalProject_Add(kernel
     BUILD_IN_SOURCE TRUE  
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/package/kernel/${QSG_CPU}/config/config .config
     BUILD_COMMAND make ARCH=${QSG_CPU} CROSS_COMPILE=${QSG_CROSS} all -j2 
-    INSTALL_COMMAND make INSTALL_PATH=${QSG_OUTPUT_DIR} install && make INSTALL_MOD_PATH=${QSG_OUTPUT_DIR} modules_install
+    INSTALL_COMMAND 
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${QSG_OUTPUT_DIR}/boot
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${QSG_OUTPUT_DIR}/lib/modules/${QSG_KERNEL_VERSION}
+        COMMAND cp ${QSG_KERNEL_DIR}/arch/${__ARCH}/boot/${__KERNEL_IMAGE} ${QSG_OUTPUT_DIR}/boot
+        COMMAND find ${QSG_KERNEL_DIR} -name "*.ko" | xargs -I {} cp {} ${QSG_OUTPUT_DIR}/lib/modules/${QSG_KERNEL_VERSION}
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 add_custom_target(kernel-compile DEPENDS kernel)
